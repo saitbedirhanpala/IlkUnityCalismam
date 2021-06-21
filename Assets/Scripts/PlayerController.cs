85b812e36ae74ef20f27c43488d351a650c5363d
SaitBedirhanPala <saitbedirhanpala@gmail.com>
2021-06-21 06:36:53 +0300

    İlk unity çalışmamın dosyaları
    bu projemde sadece hareket mekaniği üzerinde temiz kod odaklı çalışma yaptım

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;     // float bir hız değişkeni oluşturdum.
+    private float turnSpeed=45f;   // aracımın rotasyonununda değişimi için bir float dönüş hızı değişkeni oluşturdum.
+    private float horizontalInput; // yatay hareketleri tanımlayacağım bir float değişken oluşturdum.
+    private float forwardInput;    // dikey hareketleri tanımlayacağım bir float değişken oluşturdum.
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal"); //input fonksiyonu ile  yatay hareket için tuş atadım
+        forwardInput = Input.GetAxis("Vertical");      //input fonksiyonu ile  dikey hareket için tuş atadım
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput); // aracın hareket etmesi için tanımladığım kod parçacığı
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);// aracın hareket ederken dönmesi için atadığım kod parçacığı
+        
+    }
+}
